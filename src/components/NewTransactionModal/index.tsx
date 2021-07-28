02337ea977b2471dc1be21822e70d1dfb7fa7823
weilemann <yuri_weilemann@hotmail.com>
2021-07-28 11:15:30 -0300

    Created modal withdraw / deposit buttons stylizations

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import { Container, TransactionTypeContainer } from './styles';
 
 type NewTransactionModalProps = {
     isOpen: boolean;
@@ -29,14 +31,33 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     type="text" 
                     placeholder="Título" 
                 />
+
                 <input 
                     type="number" 
                     placeholder="Valor" 
                 />
+
+                <TransactionTypeContainer>
+                    <button
+                        type="button"
+                    >
+                        <img src={incomeImg} alt="Entrada" />
+                        <span>Entrada</span>
+                    </button>
+                    
+                    <button
+                        type="button"
+                    >
+                        <img src={outcomeImg} alt="Saída" />
+                        <span>Saída</span>
+                    </button>
+                </TransactionTypeContainer>
+                
                 <input 
                     type="text" 
                     placeholder="Categoria" 
                 />
+
                 <button type="submit">
                     Cadastrar
                 </button>
